3fb0c102a582f6664a6fb0a9dbf90e1dd48ee579
aula <martinconcetti97@gmail.com>
2022-05-24 15:10:20 -0300

    Testing scene and basci movement

---
+++ Bomberman/Assets/Scrips/Player_Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+
+    Vector2 movement;
+
+    private float speed = 12f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * movement.x + transform.forward * movement.y;
+
+        controller.Move(move * speed * Time.deltaTime);
+    
+    }
+}
