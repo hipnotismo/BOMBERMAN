133833a7050daca9bb5b73f9e8a508032329918f
aula <martinconcetti97@gmail.com>
2022-05-24 16:00:40 -0300

    Bomb prefab and instance

---
+++ Bomberman/Assets/CreateBomb.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBomb : MonoBehaviour
+{
+    [SerializeField] private GameObject bomb;
+    [SerializeField] private Transform playerPos;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("bomb");
+            Instantiate(bomb, playerPos.position, playerPos.rotation);
+        }
+    }
+}
