799f9644f694b493bd49b4e8a0c9f50e1dc73eb0
aula <martinconcetti97@gmail.com>
2022-05-24 16:08:17 -0300

    Bomb destroys

---
+++ Bomberman/Assets/Timer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Destroy(gameObject,5f);
+    }
+}
