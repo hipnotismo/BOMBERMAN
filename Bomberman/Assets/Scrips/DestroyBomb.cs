0d269e2f962e3285c3fe04ad2e2108d4f70f0b92
aula <martinconcetti97@gmail.com>
2022-05-31 16:51:09 -0300

    Bomb multiple raycasts

--- Bomberman/Assets/Scrips/DestroyBomb.cs
+++ Bomberman/Assets/Scrips/DestroyBomb.cs
@@ -7,27 +7,43 @@ public class DestroyBomb : MonoBehaviour
     int destroyTime = 3;
     float destroTimer =0;
     float range = 40f;
+    List<Vector3> directions = new List<Vector3>();
 
+    private void Awake()
+    {
+        directions.Add(transform.forward);
+        directions.Add(-transform.forward);
+        directions.Add(transform.right);
+        directions.Add(-transform.right);
+
+
+    }
     void Update()
     {
         RaycastHit hit;
 
         destroTimer += 1 * Time.deltaTime;
         Debug.Log(destroTimer);
+
+        
         if (destroTimer > destroyTime)
         {
-            Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
-            Debug.DrawRay(transform.position, forward, Color.green);
-
-            if (Physics.Raycast(transform.position, transform.forward,out hit, range))
+            for (int i = 0; i < 4; i++)
             {
-                Debug.Log(hit.transform.name);
-                Iterface isHit = hit.collider.GetComponent<Iterface>();
+                Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+                Debug.DrawRay(transform.position, forward, Color.green);
 
-                if (isHit != null)
+                if (Physics.Raycast(transform.position, directions[i], out hit, range))
                 {
-                    isHit.damageable();
+                    Debug.Log(hit.transform.name);
+                    Iterface isHit = hit.collider.GetComponent<Iterface>();
+
+                    if (isHit != null)
+                    {
+                        isHit.damageable();
+                    }
                 }
+
             }
 
             Destroy(gameObject);
