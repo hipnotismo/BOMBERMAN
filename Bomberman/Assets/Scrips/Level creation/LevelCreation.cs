e1e3627094db71d130e3f4f54beb0ba25ba81056
aula <martinconcetti97@gmail.com>
2022-06-14 10:17:04 -0300

    Pause prefav

--- Bomberman/Assets/Scrips/Level creation/LevelCreation.cs
+++ Bomberman/Assets/Scrips/Level creation/LevelCreation.cs
@@ -12,11 +12,9 @@ public class LevelCreation : MonoBehaviour
         for (int i = 0; i < cant; i++)
         {
             Instantiate(wall, new Vector3(i,0,0), Quaternion.identity);
-
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
