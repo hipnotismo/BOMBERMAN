c1545a1d06769dce237cd68ad2b4e37b59bf8c1d
aula <martinconcetti97@gmail.com>
2022-06-21 13:47:35 -0300

    Bomb audio fix

--- Bomberman/Assets/Scrips/Bomb/DestroyBomb.cs
+++ Bomberman/Assets/Scrips/Bomb/DestroyBomb.cs
@@ -13,7 +13,7 @@ public class DestroyBomb : MonoBehaviour
     float range = 1f;
     int RycastAmount = 4;
     float sphereRadius = 0.5f;
-
+    bool once = true;
     List<Vector3> directions = new List<Vector3>();
     [SerializeField] private LayerMask layer;
 
@@ -26,13 +26,13 @@ public class DestroyBomb : MonoBehaviour
         directions.Add(transform.right);
         directions.Add(-transform.right);
         //explosion.volume = PlayerPrefs.GetFloat("FXVolume");
+        explosion.Pause();
     }
 
     private void Update()
     {
         destroTimer += 1 * Time.deltaTime;
         explosion.volume = 1;
-        Eliminate();
 
     }
 
@@ -42,55 +42,60 @@ public class DestroyBomb : MonoBehaviour
 
 
         if (destroTimer >= destroyTime)
-        {            
-            for (int i = 0; i < RycastAmount; i++)
+        {
+            if (once==true)
             {
-
-                if (Physics.Raycast(transform.position, directions[i], out hit, range))
+                for (int i = 0; i < RycastAmount; i++)
                 {
-                    Debug.Log("Normal raycast: " + hit.collider.name);
-                    Interface isHit = hit.collider.GetComponent<Interface>();
 
-                    if (isHit != null)
+                    if (Physics.Raycast(transform.position, directions[i], out hit, range))
                     {
-                        if (hit.collider.CompareTag("Player") || hit.collider.CompareTag("Enemy"))
+                        Debug.Log("Normal raycast: " + hit.collider.name);
+                        Interface isHit = hit.collider.GetComponent<Interface>();
+
+                        if (isHit != null)
                         {
+                            if (hit.collider.CompareTag("Player") || hit.collider.CompareTag("Enemy"))
+                            {
+
+                            }
+                            else
+                            {
+                                OnBoxDestroyed?.Invoke();
+
+                            }
+                            isHit.damageable();
 
                         }
-                        else
+
+                    }
+
+                    if (Physics.SphereCast(transform.position, sphereRadius, new Vector3(0, 0, 0), out hit, 0.1f, layer, QueryTriggerInteraction.UseGlobal))
+                    {
+                        Debug.Log("Sphere raycast:" + hit.collider.name);
+                        Interface isHit = hit.collider.GetComponent<Interface>();
+                        if (isHit != null)
                         {
+                            isHit.damageable();
                             OnBoxDestroyed?.Invoke();
-
                         }
-                        isHit.damageable();
-                        
                     }
 
+                    //if (Physics.SphereCast(transform.position, sphereRadius, directions[i], out hit, 0.5f, layer, QueryTriggerInteraction.UseGlobal))
+                    //{
+                    //    Debug.Log(hit.collider.name);
+                    //    Iterface isHit = hit.collider.GetComponent<Iterface>();                
+                    //    if (isHit != null)
+                    //    {
+                    //        isHit.damageable();
+                    //        OnBoxDestroyed?.Invoke();
+                    //    }
+                    //}
+                    once = false;
                 }
-
-                if (Physics.SphereCast(transform.position, sphereRadius,new Vector3 (0,0,0), out hit, 0.1f, layer, QueryTriggerInteraction.UseGlobal))
-                {
-                    Debug.Log("Sphere raycast:" + hit.collider.name);
-                    Interface isHit = hit.collider.GetComponent<Interface>();
-                    if (isHit != null)
-                    {
-                        isHit.damageable();
-                        OnBoxDestroyed?.Invoke();
-                    }
-                }
-
-                //if (Physics.SphereCast(transform.position, sphereRadius, directions[i], out hit, 0.5f, layer, QueryTriggerInteraction.UseGlobal))
-                //{
-                //    Debug.Log(hit.collider.name);
-                //    Iterface isHit = hit.collider.GetComponent<Iterface>();                
-                //    if (isHit != null)
-                //    {
-                //        isHit.damageable();
-                //        OnBoxDestroyed?.Invoke();
-                //    }
-                //}
-
             }
+            
+            Eliminate();
 
         }
 
@@ -98,8 +103,8 @@ public class DestroyBomb : MonoBehaviour
 
     void Eliminate()
     {
-        explosion.Play();
-        Destroy(gameObject, explosion.clip.length);
+        explosion.UnPause();
+        Destroy(gameObject, 0.7f);
     }
 
     private void OnDrawGizmosSelected()
