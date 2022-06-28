a0f5a7a179cdf53f6157785f3fb1e7d7d0494c82
aula <martinconcetti97@gmail.com>
2022-06-28 15:43:29 -0300

    Sound effect for main menu

--- Bomberman/Assets/Scrips/Menu/ScreenManager.cs
+++ Bomberman/Assets/Scrips/Menu/ScreenManager.cs
@@ -1,24 +1,44 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 namespace bomberman
 {
     public class ScreenManager : MonoBehaviour
     {
         BackgroundMusic inst;
-
+        float quitTime = 1;
+        float quitTimer = 0;
         private void OnEnable()
         {
-
         }
         public void Button(string name)
         {
-            PlayerPrefs.SetString("LastScene", SceneManager.GetActiveScene().name);
-            SceneManager.LoadScene(name);
+            StartCoroutine(ButtonTimner(name));
+
         }
 
         public void ExitButton()
         {
+
+            StartCoroutine(QuitTimer());
+            
+        }
+
+        public void ReturnToLatScene()
+        {
+            StartCoroutine(ScenneChangeTimer());
+
+        }
+
+        IEnumerator QuitTimer()
+        {
+            while (quitTimer < quitTime)
+            {
+                quitTimer += Time.deltaTime;
+                yield return null;
+            }
+
             Application.Quit();
 
 #if UNITY_EDITOR
@@ -27,12 +47,29 @@ namespace bomberman
             Debug.Log("quit");
         }
 
-        public void ReturnToLatScene()
+        IEnumerator ScenneChangeTimer()
         {
+            while (quitTimer < quitTime)
+            {
+                quitTimer += Time.deltaTime;
+                yield return null;
+            }
             Debug.Log("Options");
             Debug.Log(PlayerPrefs.GetString("LastScene"));
 
             SceneManager.LoadScene(PlayerPrefs.GetString("LastScene"));
         }
+
+        IEnumerator ButtonTimner(string name)
+        {
+            while (quitTimer < quitTime)
+            {
+                quitTimer += Time.deltaTime;
+                yield return null;
+            }
+            PlayerPrefs.SetString("LastScene", SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(name);
+        }
     }
+    
 }
